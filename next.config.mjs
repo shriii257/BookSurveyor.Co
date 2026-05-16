798bab656c2e5bd1f5a9597650456de728a4e7eb
Shrinivas Biradar <biradarshri810@gmail.com>
2026-05-16 14:04:49 +0530

    Add remotePatterns configuration for images

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
